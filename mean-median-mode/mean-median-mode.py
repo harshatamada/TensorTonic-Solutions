e1495d7db98f1adb6315e3ac5866ad42460d53cb
harshatamada <166303393+harshatamada@users.noreply.github.com>
2026-09-23 19:10:08 +0530

    Solve mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import numpy as np
+
+def mean_median_mode(x: list) -> dict:
+    """
+    Returns a dictionary with mean, median, and mode.
+    """
+    x=np.asarray(x,dtype=float)
+    x.sort()
+    mean=np.mean(x)
+    median=np.median(x)
+    values,counts=np.unique(x,return_counts=True)
+    mode=values[np.argmax(counts)]
+    return {
+        "mean": float(np.mean(x)),
+        "median": float(np.median(x)),
+        "mode": float(mode),
+    }
